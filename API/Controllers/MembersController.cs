b69336a99e4f28412c4a0fb8ad42cd108bd9cb4f
omar302 <omar3025@yahoo.com>
2026-01-19 12:35:58 +1100

    Photo management  challenge complete

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -34,7 +34,8 @@ namespace API.Controllers
         [HttpGet("{id}/photos")]
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
-            return Ok(await uow.MemberRepository.GetPhotosForMemberAsync(id));
+            var isCurrentUser = User.GetMemberId() == id;
+            return Ok(await uow.MemberRepository.GetPhotosForMemberAsync(id, isCurrentUser));
         }
 
         [HttpPut]
@@ -77,12 +78,6 @@ namespace API.Controllers
                 PublicId = result.PublicId,
                 MemberId = User.GetMemberId()
             };
-
-            if (member.ImageUrl == null)
-            {
-                member.ImageUrl = photo.Url;
-                member.User.ImageUrl = photo.Url;
-            }
 
             member.Photos.Add(photo);
 
